99b4758914685d047d20302e0672e545cf0abf79
Simon Podlipsky <simon@podlipsky.net>
2019-11-23 12:16:29 +0100

    cleanup, mabye revisit

--- src/TacticianBundle.php
+++ src/TacticianBundle.php
@@ -7,7 +7,7 @@ use League\Tactician\Bundle\DependencyInjection\TacticianExtension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class TacticianBundle extends Bundle
+final class TacticianBundle extends Bundle
 {
     public function build(ContainerBuilder $container) : void
     {
