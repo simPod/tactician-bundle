5373b4b1c6f6e1733ee6387558bf071c57029d32
Simon Podlipsky <simon@podlipsky.net>
2019-11-23 12:27:35 +0100

    format

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -29,81 +29,84 @@ final class Configuration implements ConfigurationInterface
 
         $rootNode
             ->children()
-                ->arrayNode('commandbus')
-                    ->defaultValue(['default' => ['middleware' => ['tactician.middleware.command_handler']]])
-                    ->requiresAtLeastOneElement()
-                    ->useAttributeAsKey('name')
-                    ->prototype('array')
-                        ->children()
-                            ->arrayNode('middleware')
-                                ->requiresAtLeastOneElement()
-                                ->useAttributeAsKey('name')
-                                ->prototype('scalar')->end()
-                                ->validate()
-                                    ->ifTrue(
-                                        static function (array $config) : bool {
-                                        $isPresent = in_array('tactician.middleware.command_handler', $config, true);
-                                        $isLast = end($config) === 'tactician.middleware.command_handler';
+            ->arrayNode('commandbus')
+            ->defaultValue(['default' => ['middleware' => ['tactician.middleware.command_handler']]])
+            ->requiresAtLeastOneElement()
+            ->useAttributeAsKey('name')
+            ->prototype('array')
+            ->children()
+            ->arrayNode('middleware')
+            ->requiresAtLeastOneElement()
+            ->useAttributeAsKey('name')
+            ->prototype('scalar')->end()
+            ->validate()
+            ->ifTrue(
+                static function (array $config) : bool {
+                    $isPresent = in_array('tactician.middleware.command_handler', $config, true);
+                    $isLast    = end($config) === 'tactician.middleware.command_handler';
 
-                                        return $isPresent && !$isLast;
-                                    })
-                                    ->thenInvalid(
-                                        '"tactician.middleware.command_handler" should be the last middleware loaded '.
-                                        'when it is used.'
-                                    )
-                                ->end()
-                            ->end()
-                            ->scalarNode('method_inflector')->end()
-                        ->end()
-                    ->end()
-                ->end()
-                ->scalarNode('default_bus')
-                    ->defaultValue(BusBuildersFromConfig::DEFAULT_BUS_ID)
-                    ->cannotBeEmpty()
-                ->end()
-                ->scalarNode('command_handler_mapping')
-                    ->defaultValue(BusBuildersFromConfig::DEFAULT_COMMAND_HANDLER_MAPPING)
-                    ->cannotBeEmpty()
-                ->end()
-                ->arrayNode('security')
-                    ->defaultValue([])
-                    ->useAttributeAsKey('name')
-                    ->prototype('array')
-                        ->prototype('scalar')->end()
-                    ->end()
-                ->end()
-                ->scalarNode('logger_formatter')
-                    ->defaultValue('tactician.logger.class_properties_formatter')
-                    ->cannotBeEmpty()
-                ->end()
+                    return $isPresent && ! $isLast;
+                }
+            )
+            ->thenInvalid(
+                '"tactician.middleware.command_handler" should be the last middleware loaded ' .
+                'when it is used.'
+            )
+            ->end()
+            ->end()
+            ->scalarNode('method_inflector')->end()
+            ->end()
+            ->end()
+            ->end()
+            ->scalarNode('default_bus')
+            ->defaultValue(BusBuildersFromConfig::DEFAULT_BUS_ID)
+            ->cannotBeEmpty()
+            ->end()
+            ->scalarNode('command_handler_mapping')
+            ->defaultValue(BusBuildersFromConfig::DEFAULT_COMMAND_HANDLER_MAPPING)
+            ->cannotBeEmpty()
+            ->end()
+            ->arrayNode('security')
+            ->defaultValue([])
+            ->useAttributeAsKey('name')
+            ->prototype('array')
+            ->prototype('scalar')->end()
+            ->end()
+            ->end()
+            ->scalarNode('logger_formatter')
+            ->defaultValue('tactician.logger.class_properties_formatter')
+            ->cannotBeEmpty()
+            ->end()
             ->end()
             ->validate()
-                ->ifTrue(static function ($config) : bool {
+            ->ifTrue(
+                static function ($config) : bool {
                     return is_array($config) &&
                         array_key_exists('default_bus', $config) &&
-                        array_key_exists('commandbus', $config)
-                    ;
-                })
-                    ->then(static function (array $config) : array {
-                        $busNames = [];
-                        foreach ($config['commandbus'] as $busName => $busConfig) {
-                            $busNames[] = $busName;
-                        }
+                        array_key_exists('commandbus', $config);
+                }
+            )
+            ->then(
+                static function (array $config) : array {
+                    $busNames = [];
+                    foreach ($config['commandbus'] as $busName => $busConfig) {
+                        $busNames[] = $busName;
+                    }
 
-                        if (! in_array($config['default_bus'], $busNames, true)) {
-                            throw new InvalidConfigurationException(
-                                sprintf(
-                                    'The default_bus "%s" was not defined as a command bus. Valid option(s): %s',
-                                    $config['default_bus'],
-                                    implode(', ', $busNames)
-                                )
-                            );
-                        }
+                    if (! in_array($config['default_bus'], $busNames, true)) {
+                        throw new InvalidConfigurationException(
+                            sprintf(
+                                'The default_bus "%s" was not defined as a command bus. Valid option(s): %s',
+                                $config['default_bus'],
+                                implode(', ', $busNames)
+                            )
+                        );
+                    }
 
-                        return $config;
-                    })
-            ->end()
-        ;
+                    return $config;
+                }
+            )
+            ->end();
 
         return $treeBuilder;
     }
